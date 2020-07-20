12de1b86f5c225f0eaa081f073bfed65cb3bcd15
bsrcs <bsr.ercelik@gmail.com>
2020-07-20 12:26:32 +0300

    Hash and salt the password

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-// hash degeri degismez, sifreyi buraya yazinca da ayni hash degeri doner.
-console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -33,27 +31,33 @@ const User = new mongoose.model("User", userSchema);
 // kullanicinin sifresini database'de plain text olarak gorebiliriz, bu da istenilen bir sey degildir.
 /////////////////////////// Register ///////////////////////////////////////
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        //kullanicinin forma girdigi datayi aliriz.
-        email: req.body.username,
-        // password'u irreversible hash'e cevir.
-        password: md5(req.body.password)
+    //To hash a password:
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // yeni kullaniciyi sadece hash generate ettikten sonra olustururuz.
+        const newUser = new User({
+            //kullanicinin forma girdigi datayi aliriz.
+            email: req.body.username,
+            // password'u olusturdugumuz hash ile degistiririz.
+            password: hash
+        });
+        newUser.save(function (err) {
+            if (!err) {
+                // secret route'una register olduysa erisebilir, yoksa erisilemez.
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        });
     });
-    newUser.save(function (err) {
-        if (!err) {
-            // secret route'una register olduysa erisebilir, yoksa erisilemez.
-            res.render("secrets");
-        } else {
-            console.log(err);
-        }
-    });
+
+    
 });
 
 ///////////////////////////// Login ////////////////////////////////////////
 app.post("/login", function (req, res) {
     // Girilen login bilgileriyle database'deki bilginin denkligini kontrol ederim.
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     //email verisi database'de yer alirken; login yaparken username bilgisi gelir.
     //email ile username alaninin match etmesi lazim.
@@ -63,15 +67,16 @@ app.post("/login", function (req, res) {
         } else {
             // bu email ile uyusan bir kullanici varsa
             if (foundUser) {
-                //bu kullanicinin veritabanindaki hashlenmis sifresi login yaparkenki
-                // hashlenmis sifre ile uyusuyor mu
-                if (foundUser.password === password) {
-                    // kullanici adi ve sifre eslestigi icin server authenticate eder.
-                    res.render("secrets");
+                ///To check a password:
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // hashlenmis password girilen password'e esitse
+                    if(result == true){
+                        res.render("secrets");
+                    }                  
+                });
                 }
             }
-        }
-    });
+        });
 });
 
 
